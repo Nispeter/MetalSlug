a19c6ad4c39a1c2cf21d537511a1b52545145d14
Nispeter <Nispeter2@gmail.com>
2024-10-23 18:37:19 -0300

    health_pack animation fix

--- scripts/health_pack.gd
+++ scripts/health_pack.gd
@@ -1,19 +1,17 @@
 extends collectible
 
 @export var heal_value: int = 10
-@export var float_speed: float = 1.0  # Speed of floating (higher value = faster)
-@export var float_amplitude: float = 10.0  # Amplitude of floating (higher value = more movement)
+@export var float_speed: float = 1.0 
+@export var float_amplitude: float = 10.0  
 
-# Track the original position
+
 var original_y: float
 
 func _ready():
-	original_y = position.y  # Store the initial Y position
+	original_y = position.y  
 
-# This function runs every frame, creating a smooth floating effect
 func _process(delta: float) -> void:
-	# Sinusoidal motion for floating effect
-	position.y = original_y + sin(OS.get_ticks_msec() / 1000.0 * float_speed) * float_amplitude
+	position.y = original_y + sin(Time.get_ticks_msec() / 1000.0 * float_speed) * float_amplitude
 
 # Function to handle player interaction
 func _on_body_entered(body: Node) -> void:
